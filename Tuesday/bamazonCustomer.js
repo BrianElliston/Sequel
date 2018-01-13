cfd4ad94a73307ba24baf04ca11f396a728b9328
Brian Elliston <brianelliston@97-68-160-122.biz.bhn.net>
2018-01-13 14:58:18 -0500

    Everthing working except if statement

--- Tuesday/bamazonCustomer.js
+++ Tuesday/bamazonCustomer.js
@@ -1,59 +1,106 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("console.table");
+
+
+
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-  
-    
-    user: "root",
-  
-    
-    password: "",
-    database: "sql_hwkDB"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    
+  host: "localhost",
+  port: 3306,
+
+
+  user: "root",
+
+
+  password: "",
+  database: "sql_hwkDB"
+});
+
+function makeList() {
+  connection.query("SELECT * FROM products", function (err, res) {
+
+    console.table(res);
+
+
   });
 
-  
-function updateQuantity(){
-    inquirer.prompt([
-      {
-        name: "id",
-        message: "What item do you want to buy?: "
-    }, 
+}
+
+
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  makeList();
+  makePurchase();
+});
+
+
+
+
+function makePurchase() {
+  inquirer.prompt([
     {
-        name: "quantity",
-        message: "How many would you like to buy?: "
+      name: "id",
+      message: "What item do you want to buy?: "
+    },
+    {
+      name: "quantity",
+      message: "How many would you like to buy?: "
     }
-    
-    // UPDATE `a75ting`.`username` SET `points` = '`points` - 5'
-    
-    ]).then(function(answers){
-      var query = connection.query(
-        "UPDATE products SET ?",
-        {
-         
-          quantity: answers.quantity
-        },
-        function(err, res) {
-            if (err) throw err;
-            console.log(res);
-        
-        connection.end();
-        }
-      );
+
+
+
+  ])
+
+    .then(function (answers) {
+      checkQuantity(answers.quantity, answers.id);
+
     })
-  }
+}
+
+function upDateQuantity(bobQuan, bobId) {
+
+  var query = connection.query(
+
+    "UPDATE products SET quantity = quantity - ? WHERE id = ?", [bobQuan, bobId],
+
+    function (err, res) {
+      if (err) throw err;
+      console.log(res);
+      console.log("Expecting bobQuan: " + bobQuan);
+      console.log("expecting bobId: " + bobId);
+
+      makeList();
+
+      // connection.end();
+    }
+  );
+}
+
+function checkQuantity(bobQuan, bobId) {
+  connection.query("SELECT quantity FROM products WHERE id = ?", [bobId], function (err, res) {
+    if (err) throw err;
+    console.log(res);
+
+    if (parseInt(bobQuan) > parseInt(res.quantity)) {
+      console.log("Not that many in stock, please be less greedy!!")
+
+    }
+    else {
+      upDateQuantity(bobQuan, bobId);
+    }
+
+
+    connection.end();
+  })
+
+
+
+}
 
 
 
 
-
-
-
-  updateQuantity();
+// checkQuantity();
--- Tuesday/data.sql
+++ Tuesday/data.sql
@@ -8,11 +8,11 @@ CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
   items VARCHAR(45) NULL,
   price DECIMAL(10) NULL,
-  quantity INT NULL,
+  quantity INT(10) NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO  (items, price, quantity)
+INSERT INTO products (items, price, quantity)
 VALUES ("hammock", 50, 100);
 
 INSERT INTO products (items, price, quantity)
@@ -34,7 +34,7 @@ INSERT INTO products (items, price, quantity)
 VALUES ("firewood", 10, 20);
 
 INSERT INTO products (items, price, quantity)
-VALUES ("computert", 500, );
+VALUES ("computert", 500, 100);
 
 INSERT INTO products (items, price, quantity)
 VALUES ("tv", 400, 120);
