4f22037a3e2ae10404d0faa5f7383b317876cd36
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2018-01-14 09:13:27 -0500

    moved the makeList function to get the question to appear after the product list

--- Tuesday/jons.js
+++ Tuesday/jons.js
@@ -19,33 +19,27 @@ var connection = mysql.createConnection({
 });
 
 
+
 //This shows me that I'm connected 
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  
+  //Here I am calling the make list and make purchase functions
   makeList();
-  
 });
-
+/*========== I moved this to where it lists above our question =============*/
 //This function makes my list appear in the console
 function makeList() {
   connection.query("SELECT * FROM products", function (err, res) {
-
     console.table(res);
-
+    /*==== vvvv === I put this in here so it calls at the right time =============*/
     makePurchase(res);
-
   });
-
 }
 
 
-
-
-
-
-//This function is making my inquirer prompt in the console 
+//This function is making my inquirer prompt in the console
+/* ====We have to pass the vvv products to the makePurchase function ====*/ 
 function makePurchase(products) {
   inquirer.prompt([
     {
@@ -56,21 +50,17 @@ function makePurchase(products) {
       name: "quantity",
       message: "How many would you like to buy?: "
     }
-
-
-
   ])
 //This is my promise to get the user answer back before calling checkQuanity
     .then(function (answers) {
       checkQuantity(answers.quantity, answers.id);
-
     })
 }
 //Here is where I am updating my quanity
 function upDateQuantity(bobQuan, bobId) {
 
   var query = connection.query(
-//This does my math to subtract 
+
     "UPDATE products SET quantity = quantity - ? WHERE id = ?", [bobQuan, bobId],
 
     function (err, res) {
@@ -78,13 +68,11 @@ function upDateQuantity(bobQuan, bobId) {
       console.log(res);
       console.log("Expecting bobQuan: " + bobQuan);
       console.log("expecting bobId: " + bobId);
-
       makeList();
-
-    
     }
   );
 }
+
 //This is checking my quantity and running logic to let the user know if there is enough product available
 function checkQuantity(bobQuan, bobId) {
   connection.query("SELECT quantity FROM products WHERE id = ?", [bobId], function (err, res) {
@@ -106,3 +94,7 @@ function checkQuantity(bobQuan, bobId) {
 
 
 }
+
+
+
+
